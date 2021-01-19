798299c9603426dac46694e8c842270636025993
Ahmad Iqbal <48711867+ahmadiqbal1@users.noreply.github.com>
2021-01-19 05:53:31 +0500

    Initialize issue-3341

---
+++ client/app/services/notifications.js
@@ -0,0 +1,72 @@
+import { find } from 'lodash';
+import debug from 'debug';
+import recordEvent from '@/services/recordEvent';
+
+const logger = debug('redash:notifications');
+
+const Notification = window.Notification || null;
+if (!Notification) {
+  logger('HTML5 notifications are not supported.');
+}
+
+const hidden = find(
+  ['hidden', 'webkitHidden', 'mozHidden', 'msHidden'],
+  prop => prop in document,
+);
+const visibilityChange = find(
+  ['visibilitychange', 'webkitvisibilitychange', 'mozvisibilitychange', 'msvisibilitychange'],
+  prop => prop in document,
+);
+
+class NotificationsService {
+  constructor() {
+    this.pageVisible = false;
+    this.monitorVisibility();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getPermissions() {
+    if (Notification && (Notification.permission === 'default')) {
+      Notification.requestPermission((status) => {
+        if (Notification.permission !== status) {
+          Notification.permission = status;
+        }
+      });
+    }
+  }
+
+  monitorVisibility() {
+    this.pageVisible = !document[hidden];
+    let documentHidden = document[hidden];
+
+    document.addEventListener(visibilityChange, () => {
+      if (documentHidden !== document[hidden]) {
+        this.pageVisible = !document[hidden];
+        documentHidden = document[hidden];
+      }
+    });
+  }
+
+  showNotification(title, content) {
+    if (!Notification || this.pageVisible || (Notification.permission !== 'granted')) {
+      return;
+    }
+
+    // using the 'tag' to avoid showing duplicate notifications
+    const notification = new Notification(title, {
+      tag: title + content,
+      body: content,
+      icon: '/images/redash_icon_small.png',
+    });
+    setTimeout(() => {
+      notification.close();
+    }, 3000);
+    notification.onclick = function onClick() {
+      window.focus();
+      this.close();
+      recordEvent('click', 'notification');
+    };
+  }
+}
+
+export default new NotificationsService();
